848114947d2069eb60daa6a5b417e0d1e7ecd3f1
WaleedElhussain <wldelhussain@gmail.com>
2020-10-26 01:48:43 -0400

    result

---
+++ js/main.js
@@ -0,0 +1,55 @@
+// EXERCISE 1
+// Write a function named squareNumber that will take one argument (a number), square that number, and return the result. It should also log a string like "The result of squaring the number 3 is 9."
+function squareNumber(number) {
+
+}
+let number = 7;
+let result = number**2;
+console.log(`The result of squaring the number ${number} is ${result}`)
+
+
+// EXERCISE 2
+// Write a function named halfNumber that will take one argument (a number), divide it by 2, and return the result. It should also log a string like "Half of 5 is 2.5.".
+function halfNumber(numberToBeDevided){
+
+}
+let numberToBeDevided = 68;
+let result2 = numberToBeDevided/2; 
+console.log(`Half of number ${numberToBeDevided} is ${result2}`)
+   
+
+// EXERCISE 3
+// Write a function named percentOf that will take two numbers, figure out what percent the first number represents of the second number, and return the result. It should also log a string like "2 is 50% of 4."
+function percentage(num, per)
+{
+
+}
+let num=1200;
+let per = 50/100
+let result3 = num*per;
+console.log(`${result3} is 50% of the number ${num}.`);
+
+// EXERCISE 4
+// Write a function named areaOfCircle that will take one argument (the radius), calculate the area based on that, and return the result. It should also log a string like "The area for a circle with radius 2 is 12.566370614359172."
+//      Bonus: Round the result so there are only two digits after the decimal.
+function areaOfCircle (radius, pi) {
+
+   }
+let radius = 8;
+let pi = 3.1415926535898;
+let resultArea = radius**2*pi;
+console.log(`The area for a circle with radius ${radius} is ${resultArea}.`)
+
+// EXERCISE 5
+// Write a function named runAll that will take one argument (a number) and perform the following operations, using the functions you wrote earlier
+//      1. Take half of the number and store the result.
+//      2. Square the result of #1 and store that result. 
+//      3. Calculate the area of a circle with the result of #2 as the radius.   
+//      4. Calculate what percentage that area (#3) is of the squared result (#2).
+function runAll() {
+ 
+}
+console.log(`Half of number ${numberToBeDevided} is ${result2}`)
+console.log(`The result of squaring the number ${number} is ${result}`)
+console.log(`${result3} is 50% of the number ${num}.`);
+console.log(`The area for a circle with radius ${radius} is ${resultArea}.`)
